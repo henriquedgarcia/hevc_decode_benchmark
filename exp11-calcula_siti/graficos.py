2da1d107f09312d15908e7577b7087401807da67
Henrique D. Garcia <henriquedgarcia@gmail.com>
2019-05-16 00:41:51 -0300

    Atualização do resultado do siti

--- exp11-calcula_siti/graficos.py
+++ exp11-calcula_siti/graficos.py
@@ -2,9 +2,8 @@ from SITI import siti
 
 
 def main():
-    # siti.multi_plot('csv\\ninja_turtles*.csv')
     siti.multi_plot('csv\\*.csv')
-    # siti.single_plot('csv\\ninja_turtles*.csv')
+    siti.single_plot('csv\\*.csv')
 
 
 if __name__ == "__main__":
