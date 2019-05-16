b38f0cb3c9e269e73cd0600c0681a0cbee5c7c8e
Henrique D. Garcia <henriquedgarcia@gmail.com>
2019-05-15 22:59:29 -0300

    Criado script que usa o modulo SITI para calcular o siti e fazer gráficos.

---
+++ exp11-calcula_siti/graficos.py
@@ -0,0 +1,11 @@
+from SITI import siti
+
+
+def main():
+    # siti.multi_plot('csv\\ninja_turtles*.csv')
+    siti.multi_plot('csv\\*.csv')
+    # siti.single_plot('csv\\ninja_turtles*.csv')
+
+
+if __name__ == "__main__":
+    main()
