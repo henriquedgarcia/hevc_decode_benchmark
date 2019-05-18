ca6a9f918c3f7c010b5bcbc5efab47f7cadf79d1
Henrique D. Garcia <henriquedgarcia@gmail.com>
2019-05-18 14:30:00 -0300

    mergeado o script stats.py e util.py alterados no alambique

--- exp9 - compare decoder, quality factor, tiles structs/stats.py
+++ exp9 - compare decoder, quality factor, tiles structs/stats.py
@@ -1,18 +1,83 @@
 #!/bin/python3
-import os
-import numpy as np
-import matplotlib.pyplot as plt
+import itertools.product as it:
+
 from utils import util
 
 
 def main():
-    ignore = ('surf', 'om_nom', 'lions', 'rollercoaster', 'pac_man', 'clans', 'super_mario', 'ski', 'jaws',
-              'maldives', 'elephants', 'ninja_turtles', 'angels_fall', 'lion_king', 'pluto', 'ball', 'manhattan',
-              'venice')
+    # stats()
+    graph()
 
+
+def graph():
     config = util.Config('config.json')
-    dectime = util.collect_data(config=config, project='kvazaar', decoder='mp4client', ignore=ignore)
-    util.save_json(dectime, 'times.json')
+    times = util.load_json('times.json')
+    video_graph = util.VideoSegment(config=config, dectime=times)
+
+    decoder = ['ffmpeg', 'mp4client']
+    factor = ['rate', 'qp']
+    multithread = ['multi', 'single']
+
+    for (video_graph.decoder,
+         video_graph.name,
+         video_graph.fmt,
+         video_graph.factor,
+         video_graph.multithread) in it(decoder,
+                                        config.videos_list,
+                                        config.tile_list,
+                                        factor,
+                                        multithread):
+
+        # Ignore
+        if video_graph.name not in ('om_nom',
+                                    'lions',
+                                    'pac_man',
+                                    'rollercoaster'): continue
+
+        for video_graph.quality in getattr(config, f'{video_graph.factor}_list'):
+            for tiles in range(1, video_graph.num_tiles + 1):
+                video_graph.tile = tiles
+                for chunks in range(1, video_graph.duration * video_graph.fps + 1):
+                    video_graph.chunk = chunks
+
+                    size = video_graph.size
+                    ut = video_graph.times['ut']
+                    st = video_graph.times['st']
+                    rt = video_graph.times['rt']
+
+
+
+def stats():
+    # Configura os objetos
+    config = util.Config('config.json')
+
+    # Base object
+    video_seg = util.VideoSegment(config=config)
+    video_seg.project = 'ffmpeg'
+    video_seg.segment_base = 'segment'
+
+    # To iterate
+    decoders = ['ffmpeg', 'mp4client']
+    videos_list = config.videos_list
+    tile_list = config.tile_list
+    q_factors = ['rate', 'qp']
+    multithreads = [False, True]
+
+    for factors in it(decoders, videos_list, tile_list, q_factors, multithreads):
+        video_seg.decoder = factors[0]
+        video_seg.name = factors[1]
+        video_seg.fmt = factors[2]
+        video_seg.factor = factors[3]
+        video_seg.multithread = factors[4]
+
+        # Ignore
+        if video_seg.name not in ('om_nom', 'lions', 'pac_man', 'rollercoaster'):
+            continue
+
+        for video_seg.quality in getattr(config, f'{video_seg.factor}_list'):
+            times = util.collect_data(video_seg=video_seg)
+
+    util.save_json(times, 'times.json')
 
     # graph_chunk_X_time_X_tile_rate()
     # graph2()
