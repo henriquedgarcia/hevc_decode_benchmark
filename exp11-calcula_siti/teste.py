caba5b1fbdcda055a51a5c3b905ea4e545efd480
henriquedgarcia@gmail.com <henriquedgarcia@gmail.com>
2019-05-28 18:19:55 -0300

    Script para calcular o SI/TI do exp11

---
+++ exp11-calcula_siti/teste.py
@@ -0,0 +1,18 @@
+import glob
+
+from SITI import siti
+from utils import util
+
+sl = util.check_system()['sl']
+
+
+def main():
+    for video in glob.glob(f'..{sl}yuv-full{sl}*.mp4'):
+       siti.siti(filename=video)
+
+    siti.multi_plot(f'..{sl}yuv-full{sl}*.csv', output_folder='todos_os_videos_ate_agora')
+    siti.single_plot(f'..{sl}yuv-full{sl}*.csv', output_folder='todos_os_videos_ate_agora')
+
+
+if __name__ == "__main__":
+    main()
