62d3ec9e4bafa14252bb509eb4d72e25abf18d46
Henrique D. Garcia <henriquedgarcia@gmail.com>
2019-05-28 01:37:18 -0300

    Descomenta um comentário

--- exp11-calcula_siti/teste.py
+++ exp11-calcula_siti/teste.py
@@ -7,8 +7,8 @@ sl = util.check_system()['sl']
 
 
 def main():
-    #for video in glob.glob(f'..{sl}yuv-full{sl}*.mp4'):
-    #    siti.siti(filename=video)
+    for video in glob.glob(f'..{sl}yuv-full{sl}*.mp4'):
+       siti.siti(filename=video)
 
     siti.multi_plot(f'..{sl}yuv-full{sl}*.csv', output_folder='todos_os_videos_ate_agora')
     siti.single_plot(f'..{sl}yuv-full{sl}*.csv', output_folder='todos_os_videos_ate_agora')
