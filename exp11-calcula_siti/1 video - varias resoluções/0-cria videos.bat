ca854d7c38120c2f6b24e36b019b40ac5a4f41e1
Henrique D. Garcia <henriquedgarcia@gmail.com>
2019-05-28 22:49:07 -0300

    Remove resultados antigos do SITI

--- exp11-calcula_siti/1 video - varias resoluções/0-cria videos.bat
+++
@@ -1,9 +0,0 @@
-
-
-@REM off
-ffmpeg -n -i om_nom.mp4 -qp 20 -t 10 -s 3840x1920 om_nom_3840x1920_qp20.mp4
-ffmpeg -n -i om_nom.mp4 -qp 40 -t 10 -s 3840x1920 om_nom_3840x1920_qp40.mp4
-ffmpeg -n -i om_nom.mp4 -qp 20 -t 10 -s 800x400 om_nom_800x400_qp20.mp4
-ffmpeg -n -i om_nom.mp4 -qp 40 -t 10 -s 800x400 om_nom_800x400_qp40.mp4
-
-@REM FOR /L %%i IN (1,1,5) DO FOR %%p IN (*.mp4) DO ffmpeg -hide_banner -nostats -benchmark -threads 1 -i %%p -f null - 1 >> %%p.txt  2>&1
